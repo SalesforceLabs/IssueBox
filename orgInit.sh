8f63708819b63b832da1fdc50a4c98ad1b0e67f3
jrattanpal <jrattanpal@gmail.com>
2022-01-31 10:13:35 -0500

    Update package version to 1.2

--- orgInit.sh
+++ orgInit.sh
@@ -2,11 +2,9 @@
 sfdx force:org:create -f config/project-scratch-def.json  -d 3 -a IssueBoxScratchOrgPkg -s
 #sfdx force:org:create -f config/project-scratch-def.json  -d 30 -a IssueBoxScratchOrgPkg -s -v LabsDevHub
 
-#Test 2GP Package
-#sfdx force:package:install -p  -u IssueBoxScratchOrgPkg -k test1234 -w 10 -b 10
 
-#Test 1GP Package
-sfdx force:package:install -p 04t3h0000010kW5  -w 10
+#Test 1GP Package v 1.2
+sfdx force:package:install -p =04t3h0000010mmY  -w 10
 
 sfdx force:user:permset:assign -n  issuebox__IssueBox_Admin
 
